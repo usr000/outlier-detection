1e9236792fece255d37e4ac27395b895ee7920eb
alex <Alex@MBP>
2016-01-13 14:35:54 +0200

    Dockerfile relative paths issue fixed

--- rebuildAllImages.sh
+++ rebuildAllImages.sh
@@ -6,12 +6,12 @@ function mvn-there() {
   (cd $DIR; mvn "$@")     
 } 
 
-mvn-there ./spark-inbound clean compile package 
-mvn-there ./outlier-web clean compile package 
+mvn-there ./spark-inbound clean compile package && \
+mvn-there ./outlier-web clean compile package && \
 
-docker build -t outlier-detection/spark-inbound -f ./spark-inbound/docker/Dockerfile .
-docker build -t outlier-detection/outlier-web -f ./outlier-web/docker/Dockerfile .
+docker build -t outlier-detection/spark-inbound -f ./spark-inbound/docker/Dockerfile . && \
+docker build -t outlier-detection/outlier-web -f ./outlier-web/docker/Dockerfile . && \
 
-docker build -t outlier-detection/mongo-custom -f ./mongo-custom/Dockerfile . 
-docker build -t outlier-detection/python-producer -f ./python-producer/Dockerfile . 
+docker build -t outlier-detection/mongo-custom -f ./mongo-custom/Dockerfile . && \
+docker build -t outlier-detection/python-producer -f ./python-producer/Dockerfile . && \
 docker build -t outlier-detection/kafka-initializer -f ./kafka-initializer/Dockerfile .
